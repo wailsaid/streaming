faff88e0ac3e9175ffaa989fd756cac02cc30a75
wailsaid <waildebbaghi@gmail.com>
2024-08-25 18:40:32 +0100

    add plyr video player + a watch page

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/saidwail/streaming/controles"
 	"github.com/saidwail/streaming/database"
@@ -28,6 +30,16 @@ func main() {
 		c.HTML(200, "index.html", gin.H{
 			"videos": videos,
 		})
+	})
+	server.GET("/watch", func(c *gin.Context) {
+		v := c.Query("v")
+		var video models.Video
+		database.DB.Find(&video, v)
+		c.HTML(http.StatusOK, "watch.html", gin.H{"video": video})
+	})
+	server.GET("/stream", func(c *gin.Context) {
+		v := c.Query("v")
+		c.File(v)
 	})
 
 	server.GET("signup", func(c *gin.Context) {
